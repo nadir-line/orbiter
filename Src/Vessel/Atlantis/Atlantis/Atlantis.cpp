9bd18af1d56404a73aefce70d6fa07f579e71051
nadir-line <70193215+nadir-line@users.noreply.github.com>
2026-08-21 20:32:39 +0800

    Scale AoA/roll corrective rates to error
    The Atlantis flight-control logic was using a fixed ±5 deg/s correction whenever AoA or roll exceeded its limits. This change makes the target rate proportional to the amount by which the current value exceeds the allowable range, yielding smoother, more responsive corrections near the control limits.

--- Src/Vessel/Atlantis/Atlantis/Atlantis.cpp
+++ Src/Vessel/Atlantis/Atlantis/Atlantis.cpp
@@ -1811,10 +1811,10 @@ void Atlantis::clbkPreStep (double simt, double simdt, double mjd)
                 aoa_rate_error = aoa_rate_tgt - aoa_rate_curr;
 
                 if (aoa_curr > aoa_max) {
-                    aoa_rate_tgt = -5 * RAD; // Target pitch down rate of -5 deg/s
+                    aoa_rate_tgt = (aoa_max - aoa_curr) * RAD; // Target pitch down rate
                 }
                 else if (aoa_curr < aoa_min) {
-                    aoa_rate_tgt = +5 * RAD; // Target pitch up rate of +5 deg/s
+                    aoa_rate_tgt = (aoa_min - aoa_curr) * RAD; // Target pitch up rate
                 }
                 else {
                     aoa_rate_tgt = 0.0; // Target pitch rate of 0 deg/s
@@ -1851,10 +1851,10 @@ void Atlantis::clbkPreStep (double simt, double simdt, double mjd)
                 roll_rate_error = roll_rate_tgt - roll_rate_curr;
 
                 if (roll_curr > +roll_max) {
-                    roll_rate_tgt = -5 * RAD; // Target roll down rate of -5 deg/s
+                    roll_rate_tgt = (+roll_max - roll_curr) * RAD; // Target roll left rate
                 }
                 else if (roll_curr < -roll_max) {
-                    roll_rate_tgt = +5 * RAD; // Target roll up rate of +5 deg/s
+                    roll_rate_tgt = (-roll_max - roll_curr) * RAD; // Target roll right rate
                 }
                 else {
                     roll_rate_tgt = 0.0; // Target roll rate of 0 deg/s
